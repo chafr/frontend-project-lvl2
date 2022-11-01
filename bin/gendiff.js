7e9c6c04a8c8bf4937799401c7387b53998feaad
chafer <olegshteyn@gmail.com>
2022-11-01 17:20:11 +0700

    то-сё

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs'
-import _ from 'lodash';
+import { Command } from 'commander/esm.mjs';
 import printDiff from '../actions/print-diff.js';
 
-const program = new Command;
+const program = new Command();
 program
-    .description('Usage: gendiff [options] <filepath1> <filepath2>\n' +
-        'Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the current version')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action(printDiff)
-    .parse();
+  .description('Usage: gendiff [options] <filepath1> <filepath2>\n'
+        + 'Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the current version')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action(printDiff)
+  .parse();
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+[![Node CI](https://github.com/chafr/frontend-project-lvl2/workflows/Node%20CI/badge.svg)](https://github.com/chafr/frontend-project-lvl2/actions)
+
 https://asciinema.org/a/8IQgzIpJdcATx4C5CrXhmuaHU
